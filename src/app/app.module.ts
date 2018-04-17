c5c2608fc84a566baa0a35a23e6534c67ef8756e
rpnarola <rp@narola.email>
2018-04-17 19:14:12 +0530

    Commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from  '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -16,7 +18,9 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations:[
+    trigger('goals',[
+      transition('* => *',[
+          query(":enter",style({opacity:0}))
+      ])
+    ])
+
+  ]
 })
 export class HomeComponent implements OnInit {
-
+  itemCount: number;
+  btnText:string = "Add New Item";
+  GoalText:string = "My First Item";
+  goals = [];  
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    this.goals.push(this.GoalText);
+    this.GoalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
+ 
